a918ea39e95c9f213a801a05a84bd33d109cf6a9
Gianluca Arbezzano <gianarb@users.noreply.github.com>
2016-07-19 23:34:25 +0100

    Add few comments for orchestration.sh
    Add comment for each function, in order to understand what each single function do

--- orchestrate.sh
+++ orchestrate.sh
@@ -8,25 +8,23 @@ if [ -z $D_PROJECT]; then
     export D_PROJECT=ds
 fi
 
+# Create a new cluster, the first param for this function is the driver.
+# We support virtualbox and digitalocean
 function d12_cluster_create {
     ./cluster/create.sh ${1}
 }
 
+# Destroy all cluster
 function d12_cluster_destroy {
     ./cluster/destroy.sh
 }
 
+# Create base network
 function d12_create_network {
     docker network create -d overlay ${D_NETWORK}
 }
 
-function d12_run_registry() {
-    docker run -d \
-        --name ${D_PROJECT}_registry \
-         -p 5000:5000 \
-         registry
-}
-
+# Start mysql
 function d12_run_mysql() {
     docker run \
         --net ${D_NETWORK} \
@@ -34,6 +32,8 @@ function d12_run_mysql() {
         -e MYSQL_ROOT_PASSWORD=root -d mysql
 }
 
+# Run gianarb/micro the first param is the version.
+# You can use 1.0.0, 2.0.0 or latest.
 function d12_run_micro() {
     version=$1
     if [ -z $version ]; then
@@ -55,14 +55,17 @@ function d12_run_micro() {
     fi
 }
 
+# Create a dummy service that ping docker.com
 function d12_dummy_service {
     docker service create --name helloworld --replicas 1 alpine ping docker.com
 }
 
+# Scale dummy servier up from 1 to 10
 function d12_dummy_service_scale_up {
     docker service scale helloworld=10
 }
 
+# Start a new service for gianarb/micro
 function d12_micro_service() {
     version=$1
     if [ -z $version ]
